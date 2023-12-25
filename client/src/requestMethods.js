fbe8ea01306727eafe5454bed2b1c5a319cb4713
Yuda Ristian Asgari <yudaramasenju@gmail.com>
2023-12-25 10:04:26 +0700

    Update

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from "axios"
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://returnedinc-api.vercel.app/api/"
 const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODE3YWMzMTU0YTA5MGM3ZjA2ZmRiZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMzIwNDU4OCwiZXhwIjoxNzAzNDYzNzg4fQ.cHXTJ4NkfhXWL-9SgdSg9-918itJcvLX9wR5EAJ0yMA";
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODE3YWMzMTU0YTA5MGM3ZjA2ZmRiZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMzIwNDU4OCwiZXhwIjoxNzAzNDYzNzg4fQ.cHXTJ4NkfhXWL-9SgdSg9-918itJcvLX9wR5EAJ0yMA"
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
-});
+	baseURL: BASE_URL,
+})
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
-});
+	baseURL: BASE_URL,
+	header: { token: `Bearer ${TOKEN}` },
+})
